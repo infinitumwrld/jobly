ef11c2feb1ae3b4b8c339e2d367fe6a407df35d6
Carlos <mejias.alberto@yahoo.com>
2025-03-29 22:30:40 -0400

    implement interview generation

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import { isAuthenticated } from '@/lib/actions/authaction'
+import Image from 'next/image'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import React, { ReactNode } from 'react'
+
+const RootLayout = async ({ children } : { children : ReactNode}) => {
+
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect('/sing-in')
+
+  return (
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className="flex items-center gap-2" >
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className='text-primary-100'>
+              Jobly
+          </h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default RootLayout
---
+++ firebase/client.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7PXI53X3EPzJzL3Lts6s3F7-BQCM-wTY",
+  authDomain: "jobly-92ac1.firebaseapp.com",
+  projectId: "jobly-92ac1",
+  storageBucket: "jobly-92ac1.firebasestorage.app",
+  messagingSenderId: "939231843151",
+  appId: "1:939231843151:web:92dfc0621c5b51ea4b6b3a",
+  measurementId: "G-MMLBVFJ8PD"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp() ;
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app)
