6b94cfbdfe4ed00d425c8798363b9a6611686194
Carlos <mejias.alberto@yahoo.com>
2025-03-30 21:14:52 -0400

    show generated interviews on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,20 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { dummyInterviews } from '@/constants'
 import InterviewCard from '../components/InterviewCard'
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/authaction'
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all ([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! })
+  ])
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = latestInterviews?.length! > 0;
+
+
   return (
     <>
       <section className='card-cta'>
@@ -33,9 +45,13 @@ const page = () => {
 
         <div className='interviews-section'>
 
-            {dummyInterviews.map((interview) => (
-              <InterviewCard {...interview}  key={interview.id} />
-            ))}
+        { hasPastInterviews ? (
+                userInterviews?.map((interview) => (
+                  <InterviewCard {...interview}  key={interview.id} />
+                ))) :( 
+                  <p>You haven't taken any interviews yet </p>
+                )
+              } 
 
         </div>
       </section>
@@ -46,13 +62,14 @@ const page = () => {
         </h2>
 
         <div className='interviews-section'>
-          {dummyInterviews.map((interview) => (
-                <InterviewCard {...interview}  key={interview.id} />
-              ))}
-
-          {/*<p>
-            There are no recent interviews...
-          </p>*/}
+             { hasUpcomingInterviews ? (
+                latestInterviews?.map((interview) => (
+                  <InterviewCard {...interview}  key={interview.id} />
+                ))) :( 
+                  <p>There are no recent interviews... </p>
+                )
+              } 
+    
         </div>
       </section>
     </>
