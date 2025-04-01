544ddf5644701595986f36d227b02d7932f773da
Carlos <mejias.alberto@yahoo.com>
2025-04-01 01:16:06 -0400

    show feedback

--- app/components/InterviewCard.tsx
+++ app/components/InterviewCard.tsx
@@ -5,9 +5,10 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import Link from 'next/link';
 import { Button } from './ui/button';
 import DisplayTechicons from './DisplayTechicons';
+import { getFeedbackByInterviewId } from '@/lib/actions/generalaction';
 
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+  const feedback = userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId }) : null 
   const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
   const formattedDate =  dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D,YYYY'); 
 
