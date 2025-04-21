2e03196bc45ab4d6a48b608a2d5e5c4928b7aea4
Carlos <mejias.alberto@yahoo.com>
2025-04-20 23:32:46 -0400

    fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,14 +6,14 @@ import {FaLocationArrow} from 'react-icons/fa6'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-20'>
+    <div className='pb-20 pt-15'>
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
             <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
         </div>
 
-        <div className='flex justify-center relative my-20 z-10'>
+        <div className='flex justify-center relative my-20  pb-15'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center'>
                 <h2 className='badge uppercase tracking-widest text-xs  max-w-80'>
                  Skill Set Gets You There, Faster.
