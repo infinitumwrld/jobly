101c2f556445c927bce8127d76474d7ddb411e8c
Carlos <mejias.alberto@yahoo.com>
2025-06-28 17:18:36 -0400

    klk

--- app/(auth)/sing-in/page.tsx
+++ app/(auth)/sing-in/page.tsx
@@ -1,7 +1,7 @@
 import AuthForm from '@/app/components/AuthForm'
 
   const page = () => {
-    return <AuthForm type="sign-in" />
+    return <AuthForm type="sing-in" />
   };
 
 export default page;
